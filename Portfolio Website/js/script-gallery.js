c25a68835a28fcf5fe6087fea97b5aff01addf70
AbdyTekin <aabdullahtekin@gmail.com>
2023-07-26 19:30:52 +0300

    gallery implementation finished
    gallery now fully functional and just need to be more stylized.

--- Portfolio Website/js/script-gallery.js
+++ Portfolio Website/js/script-gallery.js
@@ -1,3 +1,17 @@
+let toggleButton = document.getElementById('toggleButton');
+let videoSection = document.getElementById('videoSection');
+let imageSection = document.getElementById('imageSection');
+
+toggleButton.onclick = () => {
+    if (videoSection.style.display !== 'none') {
+        videoSection.style.display = 'none';
+        imageSection.style.display = 'flex';
+    } else {
+        videoSection.style.display = 'grid';
+        imageSection.style.display = 'none';
+    }
+};
+
 let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector('.main-video video');
 let title = document.querySelector('.main-video .title');
@@ -14,3 +28,14 @@ listVideo.forEach(video => {
         };
     };
 });
+
+document.querySelectorAll('.image-grid img').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+}
